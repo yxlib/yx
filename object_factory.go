deaf17e094cf8dedd502cfeb18e1095a3d089b5e
guanjianchang <guanjianchang@dianchu.com>
2022-04-27 17:03:44 +0800

    [yx] 1. Use sync.Pool instead of ObjectPool.

--- object_factory.go
+++ object_factory.go
@@ -19,37 +19,20 @@ var (
 
 type objectWorkshop struct {
 	objType reflect.Type
-	pool    *ObjectPool
+	pool    *sync.Pool
 }
 
-func newObjectWorkshop(t reflect.Type, initReuseCnt uint64, maxCnt uint64) *objectWorkshop {
-	w := &objectWorkshop{
+func newObjectWorkshop(t reflect.Type, newFunc func() interface{}) *objectWorkshop {
+	return &objectWorkshop{
 		objType: t,
-		pool:    NewObjectPool(maxCnt),
+		pool: &sync.Pool{
+			New: newFunc,
+		},
 	}
-
-	for i := 0; i < int(initReuseCnt); i++ {
-		v := reflect.New(w.objType)
-		obj := v.Interface()
-		r, ok := obj.(Reuseable)
-		if !ok {
-			break
-		}
-
-		w.pushToPool(r)
-	}
-
-	return w
 }
 
 func (w *objectWorkshop) createObject() interface{} {
-	obj, ok := w.popFromPool()
-	if !ok {
-		v := reflect.New(w.objType)
-		obj = v.Interface()
-	}
-
-	return obj
+	return w.pool.Get()
 }
 
 func (w *objectWorkshop) reuseObject(v Reuseable) error {
@@ -57,21 +40,9 @@ func (w *objectWorkshop) reuseObject(v Reuseable) error {
 		return ErrObjFactObjIsNil
 	}
 
-	t := reflect.TypeOf(v)
-	t = t.Elem()
-	if w.objType != t {
-		return ErrObjFactNotTheSameType
-	}
-
-	return w.pushToPool(v)
-}
-
-func (w *objectWorkshop) popFromPool() (interface{}, bool) {
-	return w.pool.Get()
-}
-
-func (w *objectWorkshop) pushToPool(v Reuseable) error {
-	return w.pool.Reuse(v)
+	v.Reset()
+	w.pool.Put(v)
+	return nil
 }
 
 //=========================
@@ -94,18 +65,14 @@ func NewObjectFactory() *ObjectFactory {
 // @param initReuseCnt, the init count in the pool.
 // @param maxPoolCapacity, the max count in the pool.
 // @return error, the error.
-func (f *ObjectFactory) RegisterObject(obj Reuseable, initReuseCnt uint64, maxPoolCapacity uint64) (string, error) {
+func (f *ObjectFactory) RegisterObject(obj Reuseable, newFunc func() interface{}) (string, error) {
 	name, err := GetClassReflectName(obj)
 	if err != nil {
 		return "", ErrObjFactObjIsNil
 	}
 
 	t := reflect.TypeOf(obj)
-	err = f.createWorkshop(name, t, initReuseCnt, maxPoolCapacity)
-	if err != nil {
-		return "", err
-	}
-
+	f.createWorkshop(name, t, newFunc)
 	return name, nil
 }
 
@@ -114,10 +81,7 @@ func (f *ObjectFactory) RegisterObject(obj Reuseable, initReuseCnt uint64, maxPo
 // @return reflect.Type, the reflect type.
 // @return bool, true mean success, false mean failed.
 func (f *ObjectFactory) GetReflectType(name string) (reflect.Type, bool) {
-	f.lckWs.RLock()
-	defer f.lckWs.RUnlock()
-
-	w, ok := f.mapName2Workshop[name]
+	w, ok := f.getWorkshop(name)
 	if ok {
 		return w.objType, ok
 	}
@@ -130,10 +94,7 @@ func (f *ObjectFactory) GetReflectType(name string) (reflect.Type, bool) {
 // @return interface{}, the object is created.
 // @return error, the error.
 func (f *ObjectFactory) CreateObject(name string) (interface{}, error) {
-	f.lckWs.RLock()
-	defer f.lckWs.RUnlock()
-
-	w, ok := f.mapName2Workshop[name]
+	w, ok := f.getWorkshop(name)
 	if !ok {
 		return nil, ErrObjFactWsNotExist
 	}
@@ -146,10 +107,7 @@ func (f *ObjectFactory) CreateObject(name string) (interface{}, error) {
 // @param name, the name.
 // @return error, the error.
 func (f *ObjectFactory) ReuseObject(v Reuseable, name string) error {
-	f.lckWs.RLock()
-	defer f.lckWs.RUnlock()
-
-	w, ok := f.mapName2Workshop[name]
+	w, ok := f.getWorkshop(name)
 	if !ok {
 		return ErrObjFactWsNotExist
 	}
@@ -157,15 +115,20 @@ func (f *ObjectFactory) ReuseObject(v Reuseable, name string) error {
 	return w.reuseObject(v)
 }
 
-func (f *ObjectFactory) createWorkshop(name string, t reflect.Type, initReuseCnt uint64, maxPoolCapacity uint64) error {
+func (f *ObjectFactory) createWorkshop(name string, t reflect.Type, newFunc func() interface{}) {
 	f.lckWs.Lock()
 	defer f.lckWs.Unlock()
 
 	_, ok := f.mapName2Workshop[name]
-	if ok {
-		return ErrObjFactWsExist
+	if !ok {
+		f.mapName2Workshop[name] = newObjectWorkshop(t, newFunc)
 	}
+}
 
-	f.mapName2Workshop[name] = newObjectWorkshop(t, initReuseCnt, maxPoolCapacity)
-	return nil
+func (f *ObjectFactory) getWorkshop(name string) (*objectWorkshop, bool) {
+	f.lckWs.RLock()
+	defer f.lckWs.RUnlock()
+
+	w, ok := f.mapName2Workshop[name]
+	return w, ok
 }
