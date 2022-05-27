af29759c175eb4a3735b9ed99b8f323c76b42b84
guanjianchang <guanjianchang@dianchu.com>
2022-05-27 17:06:57 +0800

    [yx]1. Implements BuffPool with sync.Pool

--- buff_pool.go
+++ buff_pool.go
@@ -1,33 +1,70 @@
+// Copyright 2022 Guan Jianchang. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package yx
 
+import "sync"
+
 const (
-	BP_MIN_BUFF_SIZE = 32
-	BP_MAX_BUFF_SIZE = 1024
-	BP_QUEUE_STEP    = 32
+	BP_MIN_BUFF_SIZE = 64
+	BP_MAX_BUFF_SIZE = 4 * 1024
+	BP_QUEUE_STEP    = 64
 )
 
 type BuffPool struct {
-	buffQueues  []Queue
-	maxReuseCnt int
+	pool     *sync.Pool
+	buffSize uint32
 }
 
-func NewBuffPool(maxReuseCnt uint64) *BuffPool {
-	queueCnt := (BP_MAX_BUFF_SIZE-BP_MIN_BUFF_SIZE)/BP_QUEUE_STEP + 1
+func NewBuffPool(buffSize uint32) *BuffPool {
 	p := &BuffPool{
-		buffQueues:  make([]Queue, queueCnt),
-		maxReuseCnt: int(maxReuseCnt),
+		pool:     nil,
+		buffSize: buffSize,
 	}
 
-	for i := 0; i < queueCnt; i++ {
-		p.buffQueues[i] = NewSyncLimitQueue(maxReuseCnt)
+	p.pool = &sync.Pool{
+		New: p.newBuff,
 	}
 
 	return p
 }
 
-func (p *BuffPool) CreateBuff(size uint16) []byte {
-	if (size > BP_MAX_BUFF_SIZE) || (p.maxReuseCnt == 0) {
-		return make([]byte, size)
+func (p *BuffPool) Get() *[]byte {
+	return p.pool.Get().(*[]byte)
+}
+
+func (p *BuffPool) Put(buffRef *[]byte) {
+	p.pool.Put(buffRef)
+}
+
+func (p *BuffPool) newBuff() interface{} {
+	buff := make([]byte, p.buffSize)
+	return &buff
+}
+
+type BuffFactory struct {
+	poolList []*BuffPool
+}
+
+func NewBuffFactory() *BuffFactory {
+	listCnt := (BP_MAX_BUFF_SIZE-BP_MIN_BUFF_SIZE)/BP_QUEUE_STEP + 1
+	p := &BuffFactory{
+		poolList: make([]*BuffPool, listCnt),
+	}
+
+	for i := 0; i < listCnt; i++ {
+		size := uint32(BP_MIN_BUFF_SIZE + i*BP_QUEUE_STEP)
+		p.poolList[i] = NewBuffPool(size)
+	}
+
+	return p
+}
+
+func (p *BuffFactory) CreateBuff(size uint32) *[]byte {
+	if size > BP_MAX_BUFF_SIZE {
+		buff := make([]byte, size)
+		return &buff
 	}
 
 	idx := 0
@@ -38,21 +75,12 @@ func (p *BuffPool) CreateBuff(size uint16) []byte {
 		}
 	}
 
-	item, err := p.buffQueues[idx].Dequeue()
-	if err != nil {
-		fixSize := BP_MIN_BUFF_SIZE + idx*BP_QUEUE_STEP
-		return make([]byte, fixSize)
-	}
-
-	return item.([]byte)
+	buffRef := p.poolList[idx].Get()
+	return buffRef
 }
 
-func (p *BuffPool) ReuseBuff(b []byte) {
-	if p.maxReuseCnt == 0 {
-		return
-	}
-
-	size := len(b)
+func (p *BuffFactory) ReuseBuff(buffRef *[]byte) {
+	size := len(*buffRef)
 	if size < BP_MIN_BUFF_SIZE || size > BP_MAX_BUFF_SIZE {
 		return
 	}
@@ -62,5 +90,5 @@ func (p *BuffPool) ReuseBuff(b []byte) {
 	}
 
 	idx := (size - BP_MIN_BUFF_SIZE) / BP_QUEUE_STEP
-	p.buffQueues[idx].Enqueue(b)
+	p.poolList[idx].Put(buffRef)
 }
