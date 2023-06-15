edd89cff9455085c51adb6c886de039207dcc4b2
guanjianchang <niudanshui@163.com>
2023-06-16 00:19:35 +0800

    [yx] 1. refactor event.

--- event.go
+++ event.go
@@ -15,31 +15,48 @@ var (
 )
 
 type Event struct {
-	C         chan byte
-	chanClose chan byte
+	chanBroadcast chan byte
+	lck           *FastLock
+	// lck           *sync.Mutex
+	// chanClose chan byte
 }
 
 func NewEvent() *Event {
 	return &Event{
-		C:         make(chan byte, 1),
-		chanClose: make(chan byte),
+		chanBroadcast: make(chan byte, 1),
+		lck:           NewFastLock(),
+		// lck:           &sync.Mutex{},
+		// C:         make(chan byte, 1),
+		// chanClose: make(chan byte),
 	}
 }
 
-// Send event.
-func (e *Event) Send() error {
-	select {
-	case <-e.chanClose:
-		return ErrEvtClosed
-
-	default:
-		select {
-		case e.C <- 1:
-		default:
-		}
+// Broadcast event.
+func (e *Event) Broadcast() error {
+	// e.lck.Lock()
+	if err := e.lck.TryLock(0); err != nil {
+		return err
 	}
 
+	ch := e.chanBroadcast
+	e.chanBroadcast = make(chan byte, 1)
+	e.lck.Unlock()
+
+	close(ch)
 	return nil
+
+	// select {
+	// case <-e.chanClose:
+	// 	return ErrEvtClosed
+
+	// default:
+	// 	select {
+	// 	case e.C <- 1:
+	// 	default:
+	// 	}
+	// }
+
+	// return nil
 	// if e.bClosed {
 	// 	return ErrEvtClosed
 	// }
@@ -53,13 +70,28 @@ func (e *Event) Send() error {
 
 // Wait event.
 func (e *Event) Wait() error {
-	select {
-	case <-e.chanClose:
-		return ErrEvtClosed
-	case <-e.C:
+	// e.lck.Lock()
+	if err := e.lck.TryLock(0); err != nil {
+		return err
 	}
 
+	ch := e.chanBroadcast
+	e.lck.Unlock()
+
+	if ch == nil {
+		return nil
+	}
+
+	<-ch
 	return nil
+
+	// select {
+	// case <-e.chanClose:
+	// 	return ErrEvtClosed
+	// case <-e.C:
+	// }
+
+	// return nil
 	// _, ok := <-e.C
 	// if ok {
 	// 	return nil
@@ -68,27 +100,61 @@ func (e *Event) Wait() error {
 	// return ErrEvtClosed
 }
 
+func (e *Event) GetChan() chan byte {
+	// e.lck.Lock()
+	if e.lck.TryLock(0) != nil {
+		return nil
+	}
+
+	ch := e.chanBroadcast
+	e.lck.Unlock()
+
+	return ch
+}
+
 // Close the event.
 func (e *Event) Close() {
-	select {
-	case <-e.chanClose:
+	// e.lck.Lock()
+	if e.lck.TryLock(0) != nil {
 		return
-	default:
-		close(e.chanClose)
 	}
+
+	ch := e.chanBroadcast
+	e.chanBroadcast = nil
+	e.lck.Unlock()
+
+	if ch != nil {
+		close(ch)
+	}
+
+	// select {
+	// case <-e.chanClose:
+	// 	return
+	// default:
+	// 	close(e.chanClose)
+	// }
 	// if !e.bClosed {
 	// 	e.bClosed = true
 	// 	close(e.C)
 	// }
 }
 
+func (e *Event) IsClose() bool {
+	return e.GetChan() == nil
+}
+
 // Wait event until timeout.
 // @param timeoutMSec, timeout after millisecond. 0 mean check at once, only can use in one waiter scene.
 // @return error, ErrEvtWaitTimeout mean timeout.
 func (e *Event) WaitUntilTimeout(timeoutMSec uint32) error {
+	ch := e.GetChan()
+	if ch == nil {
+		return ErrEvtClosed
+	}
+
 	if timeoutMSec == 0 {
 		select {
-		case <-e.C:
+		case <-ch:
 			return nil
 		default:
 			return ErrEvtWaitTimeout
@@ -99,11 +165,7 @@ func (e *Event) WaitUntilTimeout(timeoutMSec uint32) error {
 	t := time.NewTimer(time.Millisecond * time.Duration(timeoutMSec))
 
 	select {
-	case <-e.chanClose:
-		t.Stop()
-		err = ErrEvtClosed
-
-	case <-e.C:
+	case <-ch:
 		t.Stop()
 
 	case <-t.C:
